2bab075e53337fcebb8beab300715d50d19b9107
harlveyson956-rgb <harlveyson956@gmail.com>
2026-01-12 09:58:17 +0800

    Activity 3: Two Dimensional Array

---
+++ Son_Two-Dimensional-Array/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Son_Two_Dimensional_Array
+{
+    public static class Program 
+    {
+        private static int j;
+
+        static void Main(string[] args)
+        {
+            //Complete Name: Harlvey N.Son
+
+            //Choosen Dimensions: C. 3x3
+            //Data type: int (integer)
+
+            // Initialize the 3x3 array with sample values
+            int[,] array = new int[3,3]
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+                
+            };
+            // Print the table Name
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Table Representation of 3x3 Array");
+            Console.WriteLine("| Col 0 | Col 1 | Col 2 |");
+            Console.WriteLine("---|-------|-------|-------|");
+
+            // Print each row with row header
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"Row {i} |");
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($" {array[i, j],5} |");
+                }
+                Console.WriteLine();
+            }
+
+            // Wait for user input to close the console
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ Son_Two-Dimensional-Array/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Son_Two-Dimensional-Array")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Son_Two-Dimensional-Array")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ad3d79e0-9107-447d-a3e8-25e771489609")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
